d765224f677ab7652521e7468aec2f361c82f2cc
woojung Jung <lln0302@naver.com>
2022-04-29 10:19:36 +0900

    프로젝트업로드

---
+++ src/main/java/com/campus/myapp/FinalTeamProject3Application.java
@@ -0,0 +1,13 @@
+package com.campus.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalTeamProject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalTeamProject3Application.class, args);
+	}
+
+}
