0c1511e13c8e0dd0aba4bc1ca67b0c0c3dc33da6
98woo <phantom02101@gmail.com>
2022-05-06 03:00:23 +0900

    회원가입 기능구현, 로그인 및 회원가입 css추가

--- src/main/java/com/campus/myapp/member/MemberService.java
+++ src/main/java/com/campus/myapp/member/MemberService.java
@@ -2,4 +2,10 @@ package com.campus.myapp.member;
 
 public interface MemberService {
     public MemberVO loginCheck(MemberVO vo);
+    public int memberInsert(MemberVO vo);
+    public void certifiedPhone(String usertel, int randomNumber);
+
+    public MemberVO checkId(MemberVO vo);
+    public MemberVO checkNick(MemberVO vo);
+    public MemberVO checkTel(MemberVO vo);
 }
