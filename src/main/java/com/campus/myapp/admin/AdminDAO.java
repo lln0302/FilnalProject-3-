49945352c0bd52b438a92ffbaae8e48147bb8908
didwltjr93 <96907252+didwltjr93@users.noreply.github.com>
2022-05-06 15:00:38 +0900

    양지석 커밋 테스트

--- src/main/java/com/campus/myapp/admin/AdminDAO.java
+++ src/main/java/com/campus/myapp/admin/AdminDAO.java
@@ -1,5 +1,38 @@
 package com.campus.myapp.admin;
 
-public class AdminDAO {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import com.campus.myapp.member.MemberVO;
+@Mapper
+@Repository
+public interface AdminDAO {
+	
+	//관리자페이지 회원목록 보기
+	/*
+	public List<MemberVO> adminMembers();
+	*/
+	/*
+	
+	//관리자페이지 캠핑장목록 보기
+	public List<CampVO> adminCampList();
+	
+	//관리자페이지 캠퍼모집목록 보기
+	public List<MemberVO> adminAddCamperList();
+	
+	//관리자페이지 차박지공유목록 보기
+	public List<???> adminAutoCampList();
+	
+	//관리자페이지 캠핑용품나눔목록 보기
+	public List<MemberVO> adminShareList();
+	
+	//관리자페이지 후기목록 보기
+	public List<ReviewVO> adminReviewList();
+	
+	//관리자페이지 댓글목록 보기
+	public List<CommentVO> adminCommentList();
+	
+	*/
 }
