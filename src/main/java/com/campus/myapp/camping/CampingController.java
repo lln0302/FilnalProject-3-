18a43e7750c00681625b26b1a870700b4ab8bfce
woojung Jung <lln0302@naver.com>
2022-05-10 16:18:08 +0900

    정우준 <api에서 DB저장으로 변경>, 패키지추가

--- src/main/java/com/campus/myapp/camping/CampingController.java
+++ src/main/java/com/campus/myapp/camping/CampingController.java
@@ -1,6 +1,10 @@
 package com.campus.myapp.camping;
 
+import javax.inject.Inject;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,11 +12,34 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/camping/")
 @RestController
 public class CampingController {
-	ModelAndView mav = new ModelAndView();
+	@Inject
+	CampingService service;	
 	
 	@GetMapping("/campingSearch")
-	public ModelAndView CampingSearch() {
+	public ModelAndView CampingSearch(Paging10VO pVO) {
+		ModelAndView mav = new ModelAndView();
+		
+		//레코드수
+		pVO.setTotalRecord(service.totalRecord(pVO));
+		
+		//DB에서 캠핑장 리스트 가져오기		
+		mav.addObject("list", service.campingSearch(pVO));
+		mav.addObject("pVO", pVO);
+		
 		mav.setViewName("camping/campingSearch");
 		return mav;
+	}
+	
+	@PostMapping("addplace")
+	public int addplace(@RequestBody CampingVO[] result) {
+		int n=0;
+		try {
+			for(CampingVO vo:result) {
+				n += service.addplace(vo);
+			}
+		}catch(Exception e) {
+			
+		}
+		return n;
 	}
 }	
