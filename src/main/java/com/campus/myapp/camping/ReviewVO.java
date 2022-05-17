6d9edf9e77b3db51dcf5f48bcc308fff71ae6990
daisy <gid5214@naver.com>
2022-05-17 16:23:39 +0900

    지향 - middle of review

---
+++ src/main/java/com/campus/myapp/camping/ReviewVO.java
@@ -0,0 +1,50 @@
+package com.campus.myapp.camping;
+
+public class ReviewVO {
+
+	private int reviewno;
+	private int contentID;
+	private String nickname; 
+	private String content;
+	private String createdate;
+	private String recommend;
+	
+	public int getReviewno() {
+		return reviewno;
+	}
+	public void setReviewno(int reviewno) {
+		this.reviewno = reviewno;
+	}
+	public int getContentID() {
+		return contentID;
+	}
+	public void setContentID(int contentID) {
+		this.contentID = contentID;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
+	public String getRecommend() {
+		return recommend;
+	}
+	public void setRecommend(String recommend) {
+		this.recommend = recommend;
+	}
+	
+	
+}
